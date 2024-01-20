bec00750b9943aec031f18110a0d0cf1c9873790
Angel_San <7294ashura49994@gmail.com>
2024-01-19 21:16:29 -0600

    create Trainer logic/Trainer.py

---
+++ Trainer logic/Trainer.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+
+#Team 	
+class Team:
+	def __init__(self):
+		super(Pymon).__init__()
+
+#Bag		
+class Player_Bag:
+	def Item_slot:
+	  slot1 = items
+	  super().__init__(, Crystals, Key_Items):
+
+#Player
+class Player:
+	def __init__(self, player_1, player_2, npc):
+		self.player_1 = Player_1
+		self.player_2 = Player_2
+		self.npc = NPC
+
+#Trainers
+class Trainers:
+	Route_Trainer = []
+	Gym_Trainer= []
+	Gym_Leader =["Tonatiuh", "Atl", "Itzpapalotl", "Xochiquetzal", "Quetzalcoatl", "Cihuacoatl", "Inti", "Tlalocatl"]
+	Team_Grunt = []
+	Team_Leader = []
