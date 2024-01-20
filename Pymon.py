b60c08fb37eaeea741b3ed5c795dfea86109cc73
Angel_San <7294ashura49994@gmail.com>
2024-01-19 21:15:04 -0600

    create Pymon.py

---
+++ Pymon.py
@@ -0,0 +1,15 @@
+import sys
+import pygame
+#import scikit-learn as scik
+#import tkinter as tk
+
+pygame.init()
+screen = pygame.display.set_mode((800,800))
+pygame.display.set_caption("Pymon")
+
+
+while True:
+for event in pygame.event.get():
+if event.type == QUIT:
+pygame.quit()
+sys.exit()
