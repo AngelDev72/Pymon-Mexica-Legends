566552586f8025f00d111713365c7711f999f073
Angel_San <7294ashura49994@gmail.com>
2024-01-30 18:51:38 -0600

    create Mons logic/Movepool/Moves.py

---
+++ Mons logic/Movepool/Moves.py
@@ -0,0 +1,109 @@
+import pygame
+import sys
+from Mons logic import Stats
+from Stats import Types as Tp
+
+class attack:
+  def physical():
+    moves = {
+      1: {
+        "name": "Veno Drop",
+        "type": ,
+        "description": "",
+        "power": 90,
+        "accuracy": 85,
+        "usage": 15,
+        "key": ,
+      },
+      2: {
+        "name": "Earthquake",
+        "type": ,
+        "description": ,
+        "power": 100,
+        "accuracy": 1,
+        "usage": 15,
+        "key": ,
+      },
+      3: {
+        "name": "Scorch",
+        "type": ,
+        "description": ,
+        "power": 50,
+        "accuracy": 80,
+        "usage": 20,
+        "key": ,
+      },
+      4: {
+        "name": "Chop",
+        "type": fighting,
+        "description": ,
+        "power": 75,
+        "accuracy": 85,
+        "usage": 15,
+        "key": ,
+      },
+      5: {
+        "name": "Wing Strike",
+        "type": ,
+        "description": ,
+        "power": 70,
+        "accuracy": 90,
+        "usage": 15,
+        "key": ,
+      },
+      6: {
+        "name": "Leech",
+        "type": bug,
+        "description": ,
+        "power": 55,
+        "accuracy": 85,
+        "usage": 20,
+        "key": ,
+      },
+      7: {
+        "name": "Bite",
+        "type": ,
+        "description": ,
+        "power": 65,
+        "accuracy": 90,
+        "usage": ,
+        "key": ,
+      },
+      8: {
+        "name": "",
+        "type": ,
+        "description": ,
+        "power": ,
+        "accuracy": ,
+        "usage": ,
+        "key": ,
+      },
+      9: {
+        "name": "",
+        "type": ,
+        "description": ,
+        "power": ,
+        "accuracy": ,
+        "usage": ,
+        "key": ,
+      },
+    }
+    
+  def special():
+    
+  
+class defense:
+  def defend():
+    
+
+class status:
+  def increase():
+    
+    
+  def lower():
+    
+    
+  
+class stats:
+  def condition ():
+    
