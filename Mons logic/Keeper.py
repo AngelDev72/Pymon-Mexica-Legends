f7d7ce0743f8d6b876caa80e242fe3e7a3475afd
AngelDev72 <7294ashura49994@gmail.com>
2024-04-08 21:04:29 -0600

    added keeper.py

---
+++ Mons logic/Keeper.py
@@ -0,0 +1,20 @@
+import pygame as pg
+import sys
+from Mons_logic.Mons import Mons
+
+class Keeper:
+    def __init__(self):
+        self.pymons = []
+    
+    def max_capacity:
+        max_capacity = 300
+    
+    def add_pymons(self,pymons):
+        self.pymons.append(mons)
+        
+    def remove_pymons(self,pymons):
+        
+        if mons in self.pymons:
+        self.pymons.remove(mons)
+        else:
+        pass
