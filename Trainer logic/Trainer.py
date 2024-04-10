6022350734e335bee761063885a3cb47d60a5299
AngelDev72 <7294ashura49994@gmail.com>
2024-04-09 21:20:52 -0600

    update

--- Trainer logic/Trainer.py
+++ Trainer logic/Trainer.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import random
 
 #Team 	
 class Team:
@@ -21,8 +22,98 @@ class Player:
 
 #Trainers
 class Trainers:
-	Route_Trainer = []
-	Gym_Trainer= []
-	Gym_Leader =["Tonatiuh", "Atl", "Itzpapalotl", "Xochiquetzal", "Quetzalcoatl", "Cihuacoatl", "Inti", "Tlalocatl"]
-	Team_Grunt = []
-	Team_Leader = []
+	Route_Trainer = [
+	    "Worker",
+	    "Chandler",
+	    "Astronomer",
+	    "Preist",
+	    "Scribe",
+	    "Camper",
+	    "Infant",
+	    "Gardener"
+	    ]
+	Gym_Trainer= [
+	    "Temple Diciple"
+	    ]
+	Gym_Leader =[
+	    "Tonatiuh",
+	    "Atl",
+	    "Itzpapalotl",
+	    "Xochiquetzal",
+	    "Quetzalcoatl",
+	    "Cihuacoatl",
+	    "Inti",
+	    "Tlalocatl"
+	    ]
+	Team_Grunt = [
+	    "Sect Grunt",
+	    "Sect Admin",
+	    "Sect Admin"
+	    ]
+	Team_Leader = [
+	    "Sect Leader Mictla"
+	    ]
+	female_t = [
+	    "Citlali",
+	    "Xochitl",
+	    "Itzel",
+	    "Mayahuel",
+	    "Tlalli",
+	    "Malinalli",
+	    "Coatl",
+	    "Atlacoya",
+	    "Izel",
+	    "Xilonen",
+	    "Xiloxoch",
+	    "Xiuhtonal",
+	    "Xiuhcoatl",
+	    "Citlalmina",
+	    "Chimalma",
+	    "Tzitzilin",
+	    "Nochtli",
+	    "Atlatonin",
+	    "Ayauhcihuatl",
+	    "Cuetlaxochitl",
+	    "Chicahua",
+	    "Nenetl",
+	    "Ohtli",
+	    "Tecuichpo",
+	    "Tenoch",
+	    "Tizoc",
+	    "Xochipilli",
+	    "Cipactli",
+	    "Tlaloc",
+	    "Ichtaca"
+	    ]
+	male_t = [
+	    "Cuauhtémoc",
+	    "Moctezuma",
+	    "Huitzilopochtli",
+	    "Tlatoani",
+	    "Tlacaelel",
+	    "Xipe Totec",
+	    "Tezcatlipoca",
+	    "Cuitláhuac",
+	    "Netzahualcóyotl",
+	    "Iztaccíhuatl",
+	    "Tlahuicole",
+	    "Cacamatzin",
+	    "Xolotl",
+	    "Ixtlilxochitl",
+	    "Tizoc",
+	    "Tlaloc",
+	    "Tlalpan",
+	    "Nezahualpilli",
+	    "Axayacatl",
+	    "Cuauhtli",
+	    "Quetzalcoatl",
+	    "Chalchiuhtlicue",
+	    "Tzitzimitl",
+	    "Tepoztecatl",
+	    "Xochiquetzal",
+	    "Tecpatl",
+	    "Xiuhtecuhtli",
+	    "Mictlantecuhtli",
+	    "Tlachinolli",
+	    "Tzompantli"
+	    ]
