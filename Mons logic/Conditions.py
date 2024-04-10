267767ec85b2910e301caf5cfdf76c6e238a662b
AngelDev72 <7294ashura49994@gmail.com>
2024-04-09 21:31:47 -0600

    adedd conditions.py

---
+++ Mons logic/Conditions.py
@@ -0,0 +1,10 @@
+import pygame as pg
+import sys
+from Mons_Logic.Spirit import Spirits
+from Map_logic.Cities import Temples
+
+def Temple_counter(Spirit_Sync,Temples):
+    Temple_counter = 0
+    while Temple_counter < 5:
+      
+      
