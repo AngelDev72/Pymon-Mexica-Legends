a5b6e1bf2b6d4122511180384e7fa981e63ade8f
AngelDev <7294ashura49994@gmail.com>
2024-05-19 21:28:30 -0600

    organizing concepts

--- Mons_logic/Stats.py
+++ Mons_logic/Stats.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from Mons import Mons
 
 class Stats:
   def __init__(self, hp, attack, defense, speed, sp_attk, sp_def, accuracy, evasion):
@@ -23,39 +24,59 @@ class Stats:
     self.evasion = Evasion
 
 class Type:
-  types = ["Grass", "Fire", "Water", "Bug", "Flying", "Poison", "Electric", "Rock", "Normal", "Ground", "Psychic", "Ice", "Fighting", "Dragon", "Ghost", "Dark", "Steel", "Fairy", "Beast"]
-  def __init__(self, type_name):
-    self.type = pymon_type
-    self.strong = []
-    self.weak = []
-    self.neutral = []
+    Types = [
+        "Grass",
+        "Fire",
+        "Water",
+        "Bug",
+        "Flying",
+        "Poison",
+        "Electric",
+        "Rock",
+        "Normal",
+        "Ground",
+        "Psychic",
+        "Ice",
+        "Fighting",
+        "Dragon",
+        "Ghost",
+        "Dark",
+        "Steel",
+        "Fairy",
+        "Beast"
+        ]
+    def __init__(self, type_name):
+        self.type = pymon_type
+        self.strong = []
+        self.weak = []
+        self.neutral = []
 
-  def set_relations(self, strong, weak, neutral):
-    self.strong = strong
-    self.weak = weak
-    self.neutral = neutral
+    def set_relations(self, strong, weak, neutral):
+        self.strong = strong
+        self.weak = weak
+        self.neutral = neutral
 
 class Hold:
-  def hold(self, hold):
+    def hold(self, hold):
 
 
 class Catch_rate:
-  def catch_rate(self, catch):
+    def catch_rate(self, catch):
 
 
 class Pymmon_stats:
-  def __init__(self, name, pymon_type, level, stats, types, hold, experience = 0,):
-    self.name = name
-    self.type = pymon_type
-    self.level = level
-    self.stats = stats
-    self.types = types
-    self.experience = experience
-    self.hold = hold
+    def __init__(self, name, pymon_type, level, stats, types, hold, experience = 0,):
+        self.name = name
+        self.type = pymon_type
+        self.level = level
+        self.stats = stats
+        self.types = types
+        self.experience = experience
+        self.hold = hold
     
-  def level_up(self):
-    experience_needed = self.level * 100
-  if self.experience >= experience_needed:
-    self.level += 1
-    self.experience -= experience_needed
-    print(f" {self.name} grew to level {self.level}":
+    def level_up(self):
+        experience_needed = self.level * 100
+    if self.experience >= experience_needed:
+        self.level += 1
+        self.experience -= experience_needed
+        print(f" {self.name} grew to level {self.level}":
