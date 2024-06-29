d2b4dc0a06bbdc3645f74382a8a7d0335f387287
AngelDev <7294ashura49994@gmail.com>
2024-06-29 13:10:42 -0600

    refactoring items logic

--- Trainer_logic/Items.py
+++ Trainer_logic/Items.py
@@ -1,65 +1,359 @@
 import sys
 import pygame
 import random
+from enum import Enum
 
 #Items list
-class Heal:
-	potions = ["Potion", "Super Potion ", "Hyper Potion", "Max Potion", "Full Restore"]
-	vending = ["Water", "Tea", "Cocoa"]
-	status = ["Antidote", "Ice heal", "Burn Heal", "Paralyze Heal", "Awakening"]
-	herb = ["Bitter Powder", "Energy Root", "Rivaval Herb"]
-	special = ["Revive", "Max Revive", "Elixir", "Max Elixir"]
-	
-	def __init__(self, potions, vending, status, herb, special):
-		self.potions = Potions
-		self vending = Vending
-		self.status = Status
-		self.herb = Herb
-		self.special = Special
+class Heal(Enum):
+    Potion = {
+        "name": "Potion",
+        "description": "",
+        "quantity": [],
+        "effect": []
+    }
+    Super_Potion = {
+        "name": "Super Potion",
+        "description": "",
+        "quantity": [],
+        "effect": []
+    }
+    Hyper_Potion = {
+        "name": "Hyper Potion",
+        "description": "",
+        "quantity": [],
+        "effect": []
+    }
+    Max_Potion = {
+        "name": "Max Potion",
+        "description": "",
+        "quantity": [],
+        "effect": []
+    }
+    Full_Restore = {
+        "name": "Full Restore"
+        "description": "",
+        "quantity": [],
+        "effect": []
+    }
+    Simple_Water = {
+        "name": "Simple Water",
+        "description": "",
+        "quantity": [],
+        "effect":[]
+    }
+    Tea = {
+        "name": "Tea",
+        "description": "",
+        "quantity": [],
+        "effect": []
+    }
+    Cocoa = {
+        "name": "Cocoa",
+        "description": "",
+        "quantity": [],
+        "effect": []
+    }
+    Antidote = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Ice_heal ={
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Burn_Heal = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Paralyze_Heal = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Awakening = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Bitter_Powder = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Energy_Root = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Miracle_Herb = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Revive = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Max_Revive = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Elixir = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Max_Elixir = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
 
 #Boost Items		
-class Boost:
- 	boosts = ["Miracle Seed", "Charcoal", "Mystic Water", "Silver Powder","Sharp Beak", "Silk Scarf", "Hard Stone", "Soft Sand", "Black Glasses","Black Belt", "Twisted Spoon", "Poison Barb", "Dragon Tooth", "Metal Coat", "Nevermelt Ice", "Spell Tag", "Magnet", "Fang Collar"]
- 	def __init__(self, boost):
- 			self.boost = Boost
- 			for boost in boost[0:17]:
- 				Pymon.self.attack * 1.15 = Pymon.self.attack
- 				
+class Boost(Enum):
+    Miracle_Seed = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Honorable_Flame = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Celestial_Water = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Sensitive_Claw = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Sharp_Feather = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Soft_Huipil = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Obsidian_Stone = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Ancient_Sand = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Forbbiden_Scrolls = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Warrior_Cloth = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Gifted_Eye = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Poison_Dart = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Sharp_Fang = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Iron_Chunk = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Eternal_Ice = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Omninuos_Scroll = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Magnetic_Rock = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    Fang Collar = {
+        "name":
+        "description":
+        "quantity":
+        "effect":
+    }
+    
 #Various hold
-class Special:
- 	special_item = ["Sun Orb", "Moon Orb", "Rain Orb"]
- 	
-#Shadobest  hold
-class Shadow:
- emblem = ["Blank Emblem", "Darco Emblem", "Dread Emblem", "Earth Emblem", "Fist Emblem", "Flame Emblem", "Icicle Emblem", "Insect Emblem", "Iron Emblem", "Legend Emblem", "Meadow Emblem", "Mind Emblem", "Pixie Emblem", "Salvage Emblem", "Sky Emblem", "Splash Emblem", "Spooky Emblem", "Stone Emblem", "Toxic Emblem", "Zap Emblem"]
+class Special(Enum):
+    Sun_Orb = {
+        "name":
+        "description":
+        "effect":
+    }
+    Moon_Orb = {
+        "name":
+        "description":
+        "effect":
+    }
+    Rain_Orb = {
+        "name":
+        "description":
+        "effect":
+    }
 
-  def __init__(self, emblem):
+#Shadobest  hold
+class Shadow(Enum):
+    Blank Emblem
+    Darco Emblem
+    Dread Emblem
+    Earth Emblem
+    Fist Emblem
+    Flame Emblem
+    Icicle Emblem
+    Insect Emblem
+    Iron Emblem
+    Legend Emblem
+    Meadow Emblem
+    Mind Emblem
+    Pixie Emblem
+    Salvage Emblem
+    Sky Emblem
+    Splash Emblem
+    Spooky Emblem
+    Stone Emblem
+    Toxic Emblem
+    Zap Emblem
+
 
 #Nightmane hold
-class Nightmane:
-  earrings = ["Beast Earring","Bug Earring", "Dark Earring", "Dragon Earring", "Fighting Earring", "Fire Earring", "Ice Earring", "Electric Earring", "Fairy Earring", "Flying Earring", "Ghost Earring", "Ground Earring", "Grass Earring", "Poison Earring", "Psychic Earring", "Rock Earring", "Steel Earring", "Water Earring"]
-  
-  def __init__(self, earrings):
- 
+class Nightmane(Enum):
+    Beast Earring
+    Bug Earring
+    Dark Earring
+    Dragon Earring
+    Fighting Earring
+    Fire Earring
+    Ice Earring
+    Electric Earring
+    Fairy Earring
+    Flying Earring
+    Ghost Earring
+    Ground Earring
+    Grass Earring
+    Poison Earring
+    Psychic Earring
+    Rock Earring
+    Steel Earring
+    Water Earring
+    
+
 # Sprectrabane hold
-class Spectrabane:
-	ring = ["Shock Ring", "Chill Ring", "Burn Ring", "Douse Ring"]
-	def __init__(self, ring):
-	  
+class Spectrabane(Enum):
+    Shock Ring
+    Chill Ring
+    Burn Ring
+    Douse Ring
+
 #Fruit
-class Fruit:
-  fruit = ["Orange", "Cherry", "Peach", "Banana", "Strawberry", "Pineapple", "Pear", "Grape", "Guava", "Blueberry"]
-  def __init__(self, fruit):
-	   
+class Fruit(Enum):
+    Orange
+    Cherry
+    Peach
+    Banana
+    Strawberry
+    Pineapple
+    Pear
+    Grape
+    Guava
+    Blueberry
+
 #Evolution
-class Evolution:
-  mark = ["Leaf Mark", "Flame Mark", "Water Mark", "Moon Mark", "Thunder Mark", "Sun Mark", "Night Mark", "Shiny Mark","Dawn Mark", "Ice Mark"]
+class Evolution(Enum):
+    Leaf Mark
+    Flame Mark
+    Water Mark
+    Moon Mark
+    Thunder Mark
+    Sun Mark
+    Night Mark
+    Shiny Mark
+    Dawn Mark
+    Ice Mark
 
 #Crystals
-class Crystals:
-  crystals = ["Red Crystal", "Blue Crystal", "Yellow Crystal", "Green Crystal", "White Crystal", "Black Crystal", "Diamond", "Aquamarine", "Jade", "Turquoise", "Amathist", "Ruby", "Sapphire", "Emerald", "Topaz"]
-  capture_rate = {
-	"White Crystal": 0.1,
+class Crystals(Enum):
+    Red Crystal
+    Blue Crystal
+    Yellow Crystal
+    Green Crystal
+    White Crystal
+    Black Crystal
+    Diamond
+    Aquamarine
+    Jade
+    Turquoise
+    Amathist
+    Ruby
+    Sapphire
+    Emerald
+    Topaz
+    
+    capture_rate = {
+    "White Crystal": 0.1,
 	"Yellow Crystal": 0.15
 	"Black Crystal": 0.2,
 	"Red Crystal": 0.3, # High chance Fire
@@ -75,21 +369,19 @@ class Crystals:
 	"Emerald": 0.4, # High chance Serpentix 
 	"Diamond": 1
 	}
-		def get_capture_rate(crystal, pymon_type, pymon_name):
-		 if crystal in ["Red Crystal", "Blue Crystal", "Green Crystal", "Aquamarine", "Turquoise", "Jade", "Topaz", "Amathist"]:
-		   if pymon_type in ["Fire", "Water", "Grass", "Ice", "Dragon", "Steel", "Bug", "Electric", "Ghost"]:
-		     return 0.5
-		     elif crystal in ["Ruby", "Sapphire", "Emerald"]:
-		       elif pymon_name in ["Solaris", "Lunaryx", "Serpentix"]:
-		         return 0.5
-		          else:
-		            return 
 
 #Key Items
-class key_items:
-	key = ["Instant Raft", "Lantern", "Quartzite", "Fruit Pouch", "Sharp Tooth", "Fin Bone", "Rod", "Spirit Band"]
-	def __init__(self):
+class key_items(Enum):
+    Instant Raft
+    Lantern
+    Quartzite
+    Fruit Pouch
+    Sharp Tooth
+    Fin Bone
+    Rod
+    Spirit Band
 
-#Hold item function 
-class hold:
-	def __init__(self, Heal, Boost, Special, Shadow, Spectrabane, Nightmane, Fruit):
+        "name":
+        "description":
+        "quantity":
+        "effect":
