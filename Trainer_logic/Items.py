23a9fbc9e89928960911e5274fc927b99f27c446
AngelDev <7294ashura49994@gmail.com>
2024-07-08 21:15:07 -0600

    erased code from crystls class

--- Trainer_logic/Items.py
+++ Trainer_logic/Items.py
@@ -750,24 +750,6 @@ class Crystals(Enum):
         "effect": [],
         "catch rate": []
     }
-    
-capture_rate = {
-    "White Crystal": 0.1,
-    "Yellow Crystal": 0.15
-	"Black Crystal": 0.2,
-	"Red Crystal": 0.3, # High chance Fire
-	"Blue Crystal": 0.3, # High chance Water
-	"Green Crystal": 0.3, # High chance Grass
-	"Aquamarine": 0.3, # High chance Ice
-	"Turquoise": 0.3, # High chance Steel or Dragon
-	"Jade": 0.3, # High chance Bug
-	"Topaz": 0.3, # High chance Electric 
-	"Amathist": 0.3, # High chance Ghost 
-	"Ruby": 0.4, # High chance Solaris 
-	"Sapphire": 0.4, # High chance Lunaryx
-	"Emerald": 0.4, # High chance Serpentix 
-	"Diamond": 1
-	}
 
 #Key Items
 class Key_items(Enum):
