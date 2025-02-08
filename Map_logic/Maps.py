c64a24e7914d415f970c80c17ae596e28dba0361
AngelDev <7294ashura49994@gmail.com>
2025-02-08 11:06:45 -0600

    fixing minor errors again

--- Map_logic/Maps.py
+++ Map_logic/Maps.py
@@ -5,18 +5,16 @@ class Cities(Enum):
         "name": "Tenochtitlán",
         "music theme": [],
         "map image": []
-        },
+    }
     Xaltocan = {
         "name": "Xaltocan",
         "music theme": [],
         "map image": []
-        
     }
     Teotihuacan = {
         "name": "Teotihuacán",
         "music theme": [],
         "map image": []
-    
     }
     Xochimilco = {
         "name": "Xochimilco",
@@ -28,25 +26,21 @@ class Cities(Enum):
         "name": "Chalco",
         "music theme": [],
         "map image": []
-        
     }
     Tlacopan = {
         "name": "Tlacopan",
         "music theme": [],
         "map image": []
-        
     }
     Culhuacan = {
         "name": "Culhuacan",
         "music theme": [],
         "map image": []
-        
     }
     Tenayuca = {
         "name": "Tenayuca",
         "music theme": [],
         "map image": []
-        
     }
 
 class Towns(Enum):
@@ -54,195 +48,170 @@ class Towns(Enum):
         "name": "Tepoztlán",
         "music theme": [],
         "map image": []
-    },
+    }
     Coatlinchan = {
         "name": "Coatlinchán",
         "music theme": [],
         "map image": []
-        },
+    }
     Malinalco = {
         "name": "Malinalco",
         "music theme": [],
         "map image": []
-        }
+    }
     Tlatelolco = {
         "name": "Tlatelolco",
         "music theme": [],
         "map image": []
-        }
+    }
     Chimalhuacan = {
-            "name": "Chimalhuacan",
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Chimalhuacan",
+        "music theme": [],
+        "map image": []
+    }
     Mitla = {
-            "name": "Mitla",
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Mitla",
+        "music theme": [],
+        "map image": []
+    }
     Izamal = {
-            "name": "Izamal",
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Izamal",
+        "music theme": [],
+        "map image": []
+    }
     Texcoco = {
-            "name": "Texcoco",
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Texcoco",
+        "music theme": [],
+        "map image": []
+    }
     Mixcoac = {
-            "name": "Mixcoac",
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Mixcoac",
+        "music theme": [],
+        "map image": []
+    }
 
 class Main_Routes(Enum):
     Blossom_Path = {
-            "name":"Blossom Path",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name":"Blossom Path",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Misty_River = {
-            "name": "Misty River",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Misty River",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Ancient_Trial = {
-            "name": "Ancient Trail",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": [],
-            
-            }
+        "name": "Ancient Trail",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": [],
+    }
     Whispering_Woods = {
-            "name": "Whispering Woods",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Whispering Woods",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Dragons_Pass = {
-            "name": "Dragons Pass",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Dragons Pass",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Sunset_Boulevard = {
-            "name": "Sunset Boulevard",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Sunset Boulevard",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Frostpeak_Path = {
-            "name": "Frostpeak Path",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Frostpeak Path",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Silent_Ruins = {
-            "name": "Silent Ruins",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Silent Ruins",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Crystal_Cascades = {
-            "name": "Crystal Cascades",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Crystal Cascades",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Starlight_Path = {
-            "name": "Starlight Path",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Starlight Path",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
 
 class Side_routes(Enum):
     Isle_of_Dolls = {
-            "name": "Isle of Dolls",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Isle of Dolls",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Ember_Ridge = {
-            "name": "Ember Ridge",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Ember Ridge",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Enchanted_Gorve = {
-            "name": "Enchanted Grove",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Enchanted Grove",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Coral_Reef_Path = {
-            "name": "Coral Reef Path",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Coral Reef Path",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Celestial_Bridge = {
-            "name": "Celestial Bridge",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Celestial Bridge",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
 
 class Cave(Enum):
     Secret_Chasm = {
-            "name": "Secret Chasm",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Secret Chasm",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Unexplored_Abyss = {
-            "name": "Unexplored Abyss",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-            
-        }
+        "name": "Unexplored Abyss",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Enigmatic_Passage = {
-            "name": "Enigmatic Passage",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-        
-        }
+        "name": "Enigmatic Passage",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Challenge_Cave = {
-            "name": "Challenge Cave",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-        
-        }
+        "name": "Challenge Cave",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
     Quetzalcoatl_Ruins = {
-            "name": "Quetzalcoatl Ruins",
-            "level multiplier": [],
-            "music theme": [],
-            "map image": []
-        
-        }
+        "name": "Quetzalcoatl Ruins",
+        "level multiplier": [],
+        "music theme": [],
+        "map image": []
+    }
